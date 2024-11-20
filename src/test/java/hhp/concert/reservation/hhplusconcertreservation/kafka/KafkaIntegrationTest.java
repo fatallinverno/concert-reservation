4f7e5291cb63dd1f496c973801f20be256bf0a13
Linverno <fatal_linverno@naver.com>
2024-11-21 03:19:45 +0900

    Kafka Producer, Consumer 테스트코드 작성

--- src/test/java/hhp/concert/reservation/hhplusconcertreservation/kafka/KafkaIntegrationTest.java
+++ src/test/java/hhp/concert/reservation/hhplusconcertreservation/kafka/KafkaIntegrationTest.java
@@ -1,28 +1,20 @@
 package hhp.concert.reservation.hhplusconcertreservation.kafka;
 
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 public class KafkaIntegrationTest {
 
     private static final String TOPIC = "test-topic";
-    private static final String BROKER = "127.0.0.1:9092";
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
@@ -30,33 +22,23 @@ public class KafkaIntegrationTest {
     private final CountDownLatch latch = new CountDownLatch(1);
     private String receivedMessage;
 
-    @BeforeEach
-    public void setup() {
-
-        Map<String, Object> producerProps = new HashMap<>();
-        producerProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BROKER);
-        producerProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        producerProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        ProducerFactory<String, String> producerFactory = new DefaultKafkaProducerFactory<>(producerProps);
-        kafkaTemplate = new KafkaTemplate<>(producerFactory);
-    }
-
-    @KafkaListener(topics = TOPIC, groupId = "test-group")
+    @KafkaListener(topics = TOPIC, groupId = "test")
     public void listen(String message) {
         receivedMessage = message;
         latch.countDown();
     }
 
     @Test
-    public void testKafkaProducerAndConsumer() throws InterruptedException {
+    void testKafKaProducerAndConsumer() throws InterruptedException {
 
-        String testMessage = "Kafka Test Message";
+        String testMessage = "Test Kafka Message";
         kafkaTemplate.send(TOPIC, testMessage);
 
         boolean messageReceived = latch.await(10, TimeUnit.SECONDS);
 
         assertThat(messageReceived).isTrue();
         assertThat(receivedMessage).isEqualTo(testMessage);
+
     }
 
 }
